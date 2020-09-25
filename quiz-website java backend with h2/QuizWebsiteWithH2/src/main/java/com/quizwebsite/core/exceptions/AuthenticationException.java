4c57d974caebe198da55f08abf7a09835bd4acaa
nikimar1 <32849673+nikimar1@users.noreply.github.com>
2020-09-24 20:43:11 -0400

    added a version with h2 and console enabled

---
+++ quiz-website java backend with h2/QuizWebsiteWithH2/src/main/java/com/quizwebsite/core/exceptions/AuthenticationException.java
@@ -0,0 +1,11 @@
+package com.quizwebsite.core.exceptions;
+
+public class AuthenticationException extends Exception {
+	
+	public AuthenticationException(String errorMessage)
+	{
+		super(errorMessage);
+	}
+
+
+}
---
+++ quiz-website java backend with h2/QuizWebsiteWithH2/src/test/java/com/quizwebsite/core/QuizWebsiteWithH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizwebsite.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizWebsiteWithH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
